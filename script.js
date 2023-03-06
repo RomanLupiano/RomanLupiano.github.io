129ddb19f49dbd763a8e24a7043d27501a6d9dc7
RomanLupiano <romanlupiano@gmail.com>
2023-03-06 16:52:27 -0300

    Añadido formulario de contacto y mejoras en el navbar

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var seccion1 = document.getElementById('projects');
+    var seccion2 = document.getElementById('technologies');
+    var seccion3 = document.getElementById('studies');
+    var seccion3 = document.getElementById('contact');
+  
+    if (window.pageYOffset >= seccion1.offsetTop && window.pageYOffset < seccion2.offsetTop) {
+      navbar.classList.add('fixed-top');
+      navbar.classList.remove('absolute-top');
+    } else if (window.pageYOffset >= seccion2.offsetTop && window.pageYOffset < seccion3.offsetTop) {
+      navbar.classList.add('fixed-top');
+      navbar.classList.remove('absolute-top');
+    } else if (window.pageYOffset >= seccion3.offsetTop && window.pageYOffset < seccion4.offsetTop) {
+        navbar.classList.add('fixed-top');
+        navbar.classList.remove('absolute-top');
+    } else if (window.pageYOffset >= seccion4.offsetTop) {
+      navbar.classList.remove('fixed-top');
+      navbar.classList.add('absolute-top');
+    } else {
+      navbar.classList.remove('fixed-top');
+      navbar.classList.remove('absolute-top');
+    }
+  });
+  
